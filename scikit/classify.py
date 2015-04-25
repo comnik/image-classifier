2028ebb8e95b535fe1fddf9ddc1938ce492cc95d
Nikolas Göbel <niko@clockworks.io>
2015-04-25 14:04:35 +0200

    [ADD] Some snapshotting functionality

--- scikit/classify.py
+++ scikit/classify.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3.4
-
+import pickle
 import sys
 import h5py
 import numpy as np
 
 from sklearn                import preprocessing, metrics, cross_validation, linear_model
-from sklear.decomposition   import PCA
+from sklearn.decomposition   import PCA
 
 
 def load_input(inpath):
@@ -20,9 +19,39 @@ def load_input(inpath):
         inputs = fin['data'][:]
 
         if 'label' in fin:
-            labels = fin['label'][:]
+            labels = np.ravel(fin['label'][:])
 
     return inputs, labels
+
+
+def store_input(snappath, X, Y=None):
+    """
+    Stores X and Y data in a hdf5 file.
+    """
+
+    with h5py.File(snappath, 'w') as fout:
+        fout['data'] = X
+
+        if Y is not None:
+            fout['label'] = Y
+
+
+def snapshot(model, snappath):
+    """
+    Persists a model to disk.
+    """
+
+    with open(snappath, 'wb') as fout:
+        pickle.dump(model, fout)
+
+
+def load_snapshot(snappath):
+    """
+    Returns a model that was persisted to disk.
+    """
+
+    with open(snappath, 'rb') as fin:
+        return pickle.load(fin)
 
 
 def classification_loss(Y_pred, Y_true):
@@ -48,8 +77,12 @@ def training(X, Y):
     Describes the training-steps of our classifier.
     """
 
+    print("Performing PCA...")
+
     pca = PCA(n_components=600)
     X = pca.fit_transform(X)
+
+    print("Training logistic classifier...")
 
     classifier = linear_model.LogisticRegression()
     classifier.fit(X, Y)
@@ -71,10 +104,12 @@ def main():
         Xtrain, Xtest, Ytrain, Ytest = cross_validation.train_test_split(X, Y, train_size=0.75)
         classifier = training(Xtrain, Ytrain)
 
-        scorefun = metrics.make_scorer(classification_loss)
-        scores = cross_validation.cross_val_score(classifier, X, Y, scoring=scorefun, cv=5)
+        Y_pred = classifier.predict(Xtest)
+        print(classification_loss(Y_pred, Ytest))
+        # scorefun = metrics.make_scorer(classification_loss)
+        # scores = cross_validation.cross_val_score(classifier, X, Y, scoring=scorefun, cv=5)
 
-        print('Mean: %s +/- %s' % (np.mean(scores), np.std(scores)))
+        # print('Mean: %s +/- %s' % (np.mean(scores), np.std(scores)))
 
 
 if __name__ == "__main__":
